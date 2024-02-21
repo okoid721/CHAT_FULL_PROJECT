a48440161476b7b201b556f2c3a79d3f44b13b85
okoid721 <okoid721@gmail.com>
2024-02-21 22:54:48 +0100

    Chat app full project

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -5,7 +5,9 @@ const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      res.status(401).json({ error: 'Unauthorized - No Token Provided' });
+      return res
+        .status(401)
+        .json({ error: 'Unauthorized - No Token Provided' });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
@@ -16,7 +18,7 @@ const protectRoute = async (req, res, next) => {
     const user = await User.findById(decoded.userId).select('-password');
 
     if (!user) {
-      return res.status(401).json({ error: 'User not found' });
+      return res.status(404).json({ error: 'User not found' });
     }
     req.user = user;
 
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getMessages, sendMessages } from '../controller/message.controller.js';
+import { sendMessages } from '../controller/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
-router.get('/:id', protectRoute, getMessages);
+// router.get('/:id', protectRoute, getMessages);
 router.post('/send/:id', protectRoute, sendMessages);
 
 export default router;

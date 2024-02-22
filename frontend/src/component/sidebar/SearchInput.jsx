6c8be3343ffcf67ffdfaa044e519f618710d6624
okoid721 <okoid721@gmail.com>
2024-02-22 19:56:59 +0100

    Chat app

--- frontend/src/component/sidebar/SearchInput.jsx
+++ frontend/src/component/sidebar/SearchInput.jsx
@@ -1,15 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useState} from 'react'
 import { BsFillSearchHeartFill } from "react-icons/bs";
+import useConversation from '../../zustand/useConversation'
+import useGetConversation from '../../hook/useGetConversation'
+import toast from 'react-hot-toast'
 
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("")
+  const {setSelectedConversation} = useConversation()
+  const {conversations} = useGetConversation()
+
+const handleSubmit = (e) =>{
+    e.preventDefault()
+     if(!search)return;
+      if(search.length < 3){
+      return toast.error("Search must be at least 3 character Long")
+    }
+    const conversation = conversations.find((e)=> e.fullName.toLowerCase().include(search.toLowerCase()))
+    if(conversation) {
+      setSelectedConversation(conversation)
+      setSearch('');
+    }else toast.error("Not Found")
+ }
   return (
     <div>
-         <form className="flex items-center gap-2">
+         <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <input
         type="text"
         placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         className=" input input-bordered rounded-full bg-zinc-300 placeholder:text-gray-500 text-gray-900"
       />
       <button
